b7471ed72c8ca8d2a2b54d5ca7bb6563c37f6028
Ilya.Kolotov <kolotov.iljya@gmail.com>
2022-10-17 21:54:58 +0500

    Update Task 06

--- S1_task06/Program.cs
+++ S1_task06/Program.cs
@@ -8,9 +8,9 @@ int a = Convert.ToInt32(Console.ReadLine());
 
 if (a % 2 == 0)
 {
-    Console.WriteLine($"Число {a} - четное");
+    Console.WriteLine("Четное");
 }
 else 
 {
-    Console.WriteLine($"Число {a} - нечетное");
+    Console.WriteLine("Нечетное");
 }
